462b386e4ee7ea696cf61f8b0437a3f4ab306bbc
gabriel <gabrilrenhardtdosreis@gmail.com>
2022-02-16 22:10:04 -0300

    aprendemos como funciona e como usar o controldor de dependencia do php que é o composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => 'gabriel1771/composer',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'gabriel1771/composer' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+        'monolog/monolog' => array(
+            'pretty_version' => '1.0.2',
+            'version' => '1.0.2.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../monolog/monolog',
+            'aliases' => array(),
+            'reference' => 'b704c49a3051536f67f2d39f13568f74615b9922',
+            'dev_requirement' => false,
+        ),
+    ),
+);
